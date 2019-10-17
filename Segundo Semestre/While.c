9f5cc9a7a1a7fb7b42c0151896ec0dd3cef63446
Daniela-Estupinan <55764801+Daniela-Estupinan@users.noreply.github.com>
2019-10-17 15:16:50 -0500

    Update While.c

--- Segundo Semestre/While.c
+++ Segundo Semestre/While.c
@@ -1,19 +1,21 @@
 /******************************************************************************
 
-Realizar un proyecto que permita generar n numeros enteros
+Realizar un proyecto que permita generar n numeros enteros y su sumatoria
 
 *******************************************************************************/
 #include <stdio.h>
 
 void main()
 {
-  int cont=1,nnum;
+  int cont=1,nnum,suma=0;
   printf("Cuantos numeros va a ingresar:");
   scanf("%d",&nnum);
   
   while(cont<=nnum)
   {
+      suma=suma+cont;
       printf("%d\n",cont);
       cont++;
   }
+  printf("La suma de %d numeros es %d\n",nnum,suma);
 }
